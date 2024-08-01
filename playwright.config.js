1a32132238a01f16cf26c2bf339d95de78767304
Dusan Herich <dusan.herich74@gmail.com>
2024-08-02 00:52:17 +0200

    Using reports

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,15 @@
+import { defineConfig } from '@playwright/test';
+
 module.exports = {
     use: {
         headless: true,
-
     },
 };
+
+export default defineConfig({
+    reporter: [
+        ['list'], //
+        ['html', { outputFolder: 'playwright-report' }],
+    ],
+});
+
